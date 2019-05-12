e10ebd478b9ea72a894ba7a31e8a2a651527d568
Alvinditya Saputra <alvindityas@gmail.com>
2019-05-12 19:47:40 +0700

    update database config

--- app/config/Database.py
+++ app/config/Database.py
@@ -1,10 +1,10 @@
 class DbConfig:
     def __init__(self):
-        self.DB_USERNAME = "<YOUR_USERNAME>"
-        self.DB_PASSWORD = "<YOUR_PASSWORD>"
+        self.DB_USERNAME = "maverick"
+        self.DB_PASSWORD = "maverick"
         self.DB_HOST = "localhost"
         self.DB_PORT = 3306
-        self.DB_NAME = "<YOUR_DATABASE_NAME>"
+        self.DB_NAME = "flask_mysql"
 
     def getUri(self):
         return "mysql+mysqlconnector://{}:{}@{}:{}/{}".format(self.DB_USERNAME, self.DB_PASSWORD, self.DB_HOST,
